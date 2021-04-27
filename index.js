f16a3189711537ecd78a5c50d4243b7bfc696ccd
Léo <Leo@macbook.home>
2021-04-27 15:22:51 +0200

    modification du mode de recherche (plus d'option nsfw)

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
 
-exports.search = (name, nsfw) => {
+exports.search = (name) => {
 
     try {
 
-    if(nsfw === true){
-        const result = require(`./GIFS/NSFW/${name}.json`)
-        return result
-    }else{
         const result = require(`./GIFS/${name}.json`)
         return result
-    }
+
     }catch(e){
-        console.log("une erreur est survenue")
+
+        try{
+
+        const result = require(`./GIFS/NSFW/${name}.json`)
+        return result
+
+        } catch (e) {
+
+            console.log("La requête demandée n'existe pas")
+
+        }
     }
 }
 
-exports.random = (name, nsfw) => {
+exports.random = (name) => {
 
     try {
 
-    if(nsfw === true){
-        const search = require(`./GIFS/NSFW/${name}.json`)
-        let result = search[Math.floor(Math.random() * search.length)]
-        return result
-    }else{
         const search = require(`./GIFS/${name}.json`)
-        let result = search[Math.floor(Math.random() * search.length)]
+        const result = search[Math.floor(Math.random() * search.length)]
         return result
-    }
-    }catch (e) {
-        console.log("une erreur est survenue")
+
+    }catch(e){
+
+        try{
+
+            const search = require(`./GIFS/NSFW/${name}.json`)
+            const result = search[Math.floor(Math.random() * search.length)]
+        return result
+
+        } catch (e) {
+
+            console.log("La requête demandée n'existe pas")
+            
+        }
     }
 }
